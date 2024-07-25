b74538d1d89f1a3672d6426cbad276a7df1af90f
kid <kidiatoliny@akira-io.com>
2024-07-25 14:35:15 -0100

    chore: base config

---
+++ src/Enums/SuspiciousAccountActivity.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akira\Sisp\Enums;
+
+enum SuspiciousAccountActivity: string
+{
+    case noneSuspect = '01';
+    case suspect = '02';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::noneSuspect => __('None Suspect'),
+            self::suspect => __('Suspect'),
+        };
+    }
+}
---
+++ src/Enums/TransactionCode.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akira\Sisp\Enums;
+
+enum TransactionCode: string
+{
+    case purchase = '1';
+    case servicePayment = '2';
+    case phoneRecharge = '3';
+    case enrollmentRequest = '5';
+    case tokenPurchase = '6';
+    case tokenCancel = '7';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::purchase => __('Purchase'),
+            self::servicePayment => __('Service Payment'),
+            self::phoneRecharge => __('Phone Recharge'),
+            self::enrollmentRequest => __('Enrollment Request'),
+            self::tokenPurchase => __('Token Purchase'),
+            self::tokenCancel => __('Token Cancel'),
+        };
+
+    }
+}
