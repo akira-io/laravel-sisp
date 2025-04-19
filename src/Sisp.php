2a9c36d741086cb3f069a843c56f3b81005a78c9
kid <kidiatoliny@akira-io.com>
2025-04-19 01:04:56 -0100

    chore: update PHP version requirements and dependencies, streamline CI configuration

--- src/Sisp.php
+++ src/Sisp.php
@@ -14,7 +14,6 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
 
 final class Sisp
 {
@@ -35,7 +34,7 @@ final class Sisp
      *
      * @param  array<string,mixed>  $options
      */
-    public function requestPayment(float $amount, string $transactionId, array $options = []): RedirectResponse|Redirector
+    public function requestPayment(float $amount, string $transactionId, array $options = []): RedirectResponse
     {
 
         return to_route('sisp.payment.request',
