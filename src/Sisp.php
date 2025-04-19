9c217e1f68bad8b216f03091f5cb27aca5623c5c
kid <kidiatoliny@akira-io.com>
2025-04-19 02:55:09 -0100

    chore: update return type of requestPayment method to include Redirector

--- src/Sisp.php
+++ src/Sisp.php
@@ -14,6 +14,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 
 final class Sisp
 {
@@ -34,7 +35,7 @@ final class Sisp
      *
      * @param  array<string,mixed>  $options
      */
-    public function requestPayment(float $amount, string $transactionId, array $options = []): RedirectResponse
+    public function requestPayment(float $amount, string $transactionId, array $options = []): RedirectResponse|Redirector
     {
 
         return to_route('sisp.payment.request',
