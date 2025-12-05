f4dd578ac4f63a8e7f0545ba8e39d824f2b83c10
kid <kidiatoliny@akira-io.com>
2025-12-05 01:46:00 +0100

    feat: add localization support for payment messages in French and Portuguese

---
+++ resources/lang/en/payment.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'redirect_title' => 'Redirecting to SISP',
+    'redirect_description' => 'You will be redirected to the secure payment portal',
+    'secure_transaction' => 'Secure Transaction',
+    'official_portal' => 'Official portal of Cape Verde Banking System',
+    'ssl_encryption' => 'Bank-level SSL encryption',
+    'data_protected' => 'Your data is protected',
+    'redirecting_in' => 'Redirecting in :count second|Redirecting in :count seconds',
+    'connecting' => 'Connecting to SISP...',
+];
