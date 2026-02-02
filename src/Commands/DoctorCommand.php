1c23702d8fd5d858d60b32039c099ebef95546eb
kid <kidiatoliny@gmail.com>
2026-02-02 22:37:11 +0100

    fix: correct transaction status output in doctor command and add missing properties to Invoice model

--- src/Commands/DoctorCommand.php
+++ src/Commands/DoctorCommand.php
@@ -128,7 +128,7 @@ final class DoctorCommand extends Command
                 $this->line("    Invoice: <info>#{$sample->invoice_number}</info>");
                 $this->line("    Customer: <info>{$sample->customer_name}</info>");
                 $this->line("    Status: <info>{$sample->status->value}</info>");
-                $this->line("    Transaction Status: <info>{$sample->transaction?->status->value}</info>");
+                $this->line("    Transaction Status: <info>{$sample->transaction->status->value}</info>");
                 $this->line("    Created: <info>{$sample->created_at}</info>");
             }
 
