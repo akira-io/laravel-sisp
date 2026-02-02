dbbfdc38396408b5925450a8ec601ee1c4560302
kid <kidiatoliny@gmail.com>
2026-02-03 00:06:39 +0100

    fix: correct middleware configuration for refund route and add debugging dependency

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('sisp/cancel', CancelTransactionController::class)
     ->name('sisp.cancel');
 
 Route::post('sisp/refund/{transaction}', RefundTransactionController::class)
-    ->middleware(config('sisp.middleware.refund', ['web', 'auth']))
+    ->middleware(config()->string('sisp.middleware.refund', ['web', 'auth']))
     ->name('sisp.refund');
 
 Route::match(['get', 'post'], 'sisp/sandbox', SandboxController::class)
