62357efd6c0ad2a2a7b1ca3a1223ea6dabea7fd4
kid <kidiatoliny@gmail.com>
2026-05-23 00:21:56 +0200

    test(install): back up published directories

--- tests/Commands/LaravelSispInstallCommandForceTest.php
+++ tests/Commands/LaravelSispInstallCommandForceTest.php
@@ -10,23 +10,27 @@ it('publishes inertia/vue/assets/blade with force', function (): void {
         'sisp-inertia-components' => [
             'source' => __DIR__.'/../../resources/js/react/pages/payment-response.tsx',
             'target' => resource_path('js/pages/sisp/payment-response.tsx'),
+            'targetDirectory' => resource_path('js/pages/sisp'),
         ],
         'sisp-vue-components' => [
             'source' => __DIR__.'/../../resources/js/vue/pages/PaymentResponse.vue',
             'target' => resource_path('js/pages/sisp/PaymentResponse.vue'),
+            'targetDirectory' => resource_path('js/pages/sisp'),
         ],
         'sisp-assets' => [
             'source' => __DIR__.'/../../resources/css/sisp.css',
             'target' => public_path('vendor/sisp/css/sisp.css'),
+            'targetDirectory' => public_path('vendor/sisp/css'),
         ],
         'sisp-views' => [
             'source' => __DIR__.'/../../resources/views/payment-response.blade.php',
             'target' => resource_path('views/vendor/sisp/payment-response.blade.php'),
+            'targetDirectory' => resource_path('views/vendor/sisp'),
         ],
     ];
 
     withInstallCommandFsLock(function () use ($targets): void {
-        withPublishedPathBackups(array_column($targets, 'target'), function () use ($targets): void {
+        withPublishedPathBackups(array_unique(array_column($targets, 'targetDirectory')), function () use ($targets): void {
             foreach ($targets as $tag => $paths) {
                 File::ensureDirectoryExists(dirname($paths['target']));
                 file_put_contents($paths['target'], 'stale');
--- tests/Commands/LaravelSispInstallCommandPublishTest.php
+++ tests/Commands/LaravelSispInstallCommandPublishTest.php
@@ -10,23 +10,27 @@ it('publishes blade views, inertia components, vue components and assets', funct
         'sisp-views' => [
             'source' => __DIR__.'/../../resources/views/payment-form.blade.php',
             'target' => resource_path('views/vendor/sisp/payment-form.blade.php'),
+            'targetDirectory' => resource_path('views/vendor/sisp'),
         ],
         'sisp-inertia-components' => [
             'source' => __DIR__.'/../../resources/js/react/pages/payment-form.tsx',
             'target' => resource_path('js/pages/sisp/payment-form.tsx'),
+            'targetDirectory' => resource_path('js/pages/sisp'),
         ],
         'sisp-vue-components' => [
             'source' => __DIR__.'/../../resources/js/vue/pages/PaymentForm.vue',
             'target' => resource_path('js/pages/sisp/PaymentForm.vue'),
+            'targetDirectory' => resource_path('js/pages/sisp'),
         ],
         'sisp-assets' => [
             'source' => __DIR__.'/../../resources/css/sisp.css',
             'target' => public_path('vendor/sisp/css/sisp.css'),
+            'targetDirectory' => public_path('vendor/sisp/css'),
         ],
     ];
 
     withInstallCommandFsLock(function () use ($targets): void {
-        withPublishedPathBackups(array_column($targets, 'target'), function () use ($targets): void {
+        withPublishedPathBackups(array_unique(array_column($targets, 'targetDirectory')), function () use ($targets): void {
             foreach ($targets as $tag => $paths) {
                 File::delete($paths['target']);
 
